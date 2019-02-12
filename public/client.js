59d6ea09d7c47d17000892b7ddf0b39af05a1dc9
Glitch (election-day) <none>
2019-02-12 05:31:06 +0000

    😂🌆 Updated with Glitch

--- public/client.js
+++ public/client.js
@@ -1,4 +1,32 @@
-let undoCounter = 0;
+// STEP 1: user exes out, triggering a setTimeout, which is assigned the number 1 (x)
+//         also triggers an undo button, whose id is also 1 (undoCounter)
+
+// STEP 2: if user exes out a second item, triggers a second setTimeout, which is now 2 (x)
+//         and undo button for that exout has an id of 2 (undoCounter)
+
+// STEP 3: say user wants to undo the second exout
+//         second undo is pressed
+//         the id of the undo button is captured from the click event (2)
+//         clearTimeout(2) is called, which cancels the second exout, and restores the information
+
+// STEP 4: say 3 seconds still has not passed, and user wants to undo first exout as well
+//         second undo is pressed
+//         the id of the undo button is captured from the click event (1)
+//         clearTimeout(1) is called, which cancels the first exout and restores the information
+
+// STEP 5: say user wants to exout an item and keep it exed out
+//         setTimoeut is called, which is now 3 (x)
+//         the id of the undo button is 3 (undoCounter)
+//         after 3 seconds, setTimeout runs, which 
+
+// counter that keeps track of which undo button we're pressing, and thus which information to repopulate
+let undoCounter = 1;
+
+// which setTimeout we're on, so we can clearTimeout for the correct information when we press "undo"
+let x = 1;
+
+// 
+let num = 1;
 
 // exout a section (needs to use document.on to account for sections which are added dynamically)
 $(document).on("click", ".fa-times-circle", (e) => {
@@ -11,26 +39,36 @@ $(document).on("click", ".fa-times-circle", (e) => {
 
   sectionToHide.after('<button class="undo" id="undo' + undoCounter + '" style="display: block;">Undo deletion</button>');
   undoCounter++;
-  
-  setTimeout(() => {
+
+  console.log("first num: ", num);
+  console.log("first x : ", x);
+  x = setTimeout(() => {
     
-    console.log("one");
-    $("#undo" + undoCounter).remove();
-    console.log("two");
+    // not sure why, but this has to be called twice to work
+    
+    let a = $("#undo" + (undoCounter - 1));
+    console.log(a);
+    $("#undo" + (undoCounter - 1)).remove();
+    $("#undo" + (undoCounter - 1)).remove();
+
     sectionToHide.remove();
-    console.log("three");
     
     $.ajax({
       url: "/delete",
       data: { clickedId },
       success: () => { console.log("success") },
       failure: () => { console.log("failure") }
-    })
-  }, 3000);
+    });
+  }, 4000);
   
 });
 
 $(document).on("click", ".undo", (e) => {
+  
+  num = e.target.id.split("undo")[1];
+  
+  clearTimeout(num);
+  
   // STEP 2: ADD classes back and show section again in if undo is clicked
   let clickedId = e.target.id;
   
@@ -39,16 +77,14 @@ $(document).on("click", ".undo", (e) => {
 
   prev.show();
   prevchildren.show();
-  
-  let children = prevchildren.children();
-  
-  // add classes back in
-  $(children[3]).addClass("legSection");
-  $(children[4]).addClass("plainSection");
-  $(children[5]).addClass("fiscTenSection");
+
   
   // I don't know why, but this needs to be called TWICE to remove the actual undo button ?????
   $("#" + clickedId).remove();
   $("#" + clickedId).remove();
 
 });
+
+const removeFunction = () => {
+  
+}
--- routes.js
+++ routes.js
@@ -1,12 +1,12 @@
 // libraries
-// const bcrypt = require("bcryptjs");
-// const { check, validationResult } = require('express-validator/check');
-// const fs = require("fs");
-// const jwt = require("jsonwebtoken");
-// const nodegit = require("nodegit");
-// const path = require("path"); // native to Node
-// const passport = require("passport");
-// const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require('express-validator/check');
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
+const nodegit = require("nodegit");
+const path = require("path"); // native to Node
+const passport = require("passport");
+const mongoose = require('mongoose');
 const mongo = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectID;
 
@@ -15,55 +15,93 @@ const ObjectId = require('mongodb').ObjectID;
 module.exports = (app, db) => {
   
   mongo.connect(process.env.DATABASE, (err, client) => {
-    if(err) console.log('Database error: ' + err);
+    if(err) { console.log('Database error: ' + err) }
+    
+    else {
 
-    let db = client.db('freecodecamp2018');
+      let db = client.db('freecodecamp2018');
 
-    app.get('/', (req, res) => {
-
-      db.collection('mendoza').find( { "precinct": 11 }, {sort: { name_last: 1 } }, (err, cursor) => {
-
-        if (err) { console.log(err); }
-        else {
-
-          let arr = [];
-
-          cursor.toArray()
-            .then((docs) => {
-            for (let doc of docs) {
-
-              let obj = {};
-
-              obj.lastname = doc.name_last;
-              obj.firstname = doc.name_first;
-              obj.address = doc.address;
-              obj._id = doc._id;
-
-
-              arr.push(obj);
-            }
-
-            res.render(process.cwd() + '/views/pug/index', { arr });
-
-          });
-
-        }
+      app.get('/', mainpageMiddleware, (req, res) => {
+        console.log(req.body.ward);
+        db.collection('negron').find( { }, {sort: { lastname: 1 } }, (err, cursor) => {
+          if (err) { console.log(err); }
+          else {
+            let arr = [];
+            cursor.toArray()
+              .then((docs) => {
+                for (let doc of docs) {
+                  let obj = {};
+                  obj.lastname = doc.lastname;
+                  obj.firstname = doc.firstname;
+                  obj.address = doc.address;
+                  obj._id = doc._id;
+                  arr.push(obj);
+                }
+              res.render(process.cwd() + '/views/pug/index', { arr });
+            });
+          }
+        });
       });
-    });
-    
-    
-    app.get("/delete", (req, res) => {
-      
-      console.log(req.query.clickedId);
-      
-      db.collection('mendoza').deleteOne({ _id: ObjectId(req.query.clickedId) }, true, (err, doc) => {
-        if (err) { console.log(err) }
-        else {
-          console.log("success : ", err);
-        }
-      })
-      
-    })
 
+
+      app.get("/delete", (req, res) => {
+        db.collection('negron').deleteOne({ _id: ObjectId(req.query.clickedId) }, true, (err, doc) => {
+          if (err) { console.log(err) }
+          else {
+            console.log("success : ", err);
+          }
+        })
+      });
+
+
+      app.get('/login', loginpageMiddleware, (req, res, next) => {
+        res.render(process.cwd() + "/views/login.pug");
+      });
+
+
+      app.post('/login', (req, res, next) => {
+        passport.authenticate("local", {
+          successRedirect: "/",
+          failureRedirect: "/login",
+          failureFlash: true
+        })(req, res, next);
+      });
+
+      app.get("/logout", (req, res) => {
+        req.logout();
+        res.redirect("/login");
+      });
+
+      
+      // app.get("/upload", (req, res) => {
+      //   db.collection('negron').insertMany([], (err, doc) => {
+      //     if (err) { console.log(err) }
+      //     else {
+      //       console.log("success!");
+      //       res.render(process.cwd() + "/views/login.pug");
+      //     }
+      //   })
+      // });
+    }
   })
+}
+
+// only for /login page (if person is logged in, it will redirect to main page rather than render login page)
+const loginpageMiddleware = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    res.redirect("/");
+  }
+  else {
+    next();
+  }
+}
+
+// only for "/" page (if person is not logged in, it will redirect to login page rather than render main page)
+const mainpageMiddleware = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+  }
+  else {
+    res.redirect("/login");
+  }
 }
